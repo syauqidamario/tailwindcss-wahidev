a2244d3a6f1327ba0600fd3b7eca0bd325194130
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-11-03 20:15:00 +0700

    First commit.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans:["Satoshi", "cursive"],
+      },
+      colors: {
+       primary1:"#003E29",
+       primary2:"#E4BBA1",
+       primary3: "#D9D9D9",
+       borderColor: "#787878",
+       bgColor:"#EDEDED",
+       bgGreen:"#023423",
+      }
+    },
+  },
+  plugins: [],
+};
