dc9580db3d04acd2c7822b1dd61d2e597b5d36ed
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-11-04 17:00:49 +0700

    Navbar done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans:["Satoshi", "cursive"],
+        sans: ["Satoshi", "cursive"],
       },
       colors: {
-       primary1:"#003E29",
-       primary2:"#E4BBA1",
-       primary3: "#D9D9D9",
-       borderColor: "#787878",
-       bgColor:"#EDEDED",
-       bgGreen:"#023423",
-      }
+        primary1: "#003E29",
+        primary2: "#E4BBA1",
+        primary3: "#D9D9D9",
+        borderColor: "#787878",
+        bgColor: "#EDEDED",
+        bgGreen: "#023423",
+      },
     },
   },
   plugins: [],
